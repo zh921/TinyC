5c97a7913bf2fec598b03c4672c03e4cc5404423
zh921 <18621621921@163.com>
2021-12-22 14:53:55 +0800

    update TinyC

--- submit_to_git.bat
+++ submit_to_git.bat
@@ -1,22 +1,22 @@
 @REM git remote -v
 @REM git remote remove origin
-@REM git remote add github https://github.com/zh921/Notes.git
-@REM git remote add gitee https://gitee.com/zh921/Notes.git
+@REM git remote add github https://github.com/zh921/TinyC.git
+@REM git remote add gitee https://gitee.com/zh921/TinyC.git
 
 git config user.name "zh921"
 git config user.email "18621621921@163.com"
 
 git add .
-git commit -m "add TinyC"
+git commit -m "update TinyC"
 
-git pull origin main
-git push origin main
+@REM git pull origin main
+@REM git push origin main
 
-@REM git pull github main
-@REM git push github main
+git pull github main
+git push github main
 
-@REM git pull gitee main
-@REM git push gitee main
+git pull gitee main
+git push gitee main
 
 git config user.name "hangzhao"
 git config user.email "hangzhao@amd.com"
